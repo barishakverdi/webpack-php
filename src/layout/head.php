cc47d85666ea0f429d196fa4fb900dc32b61b3ba
barishakverdi <info@barishakverdi.com>
2024-09-30 13:07:16 +0300

    firefox fouc fix

--- src/layout/head.php
+++ src/layout/head.php
@@ -10,9 +10,25 @@ $assets = '../../public/assets/';
     <title>Title</title>
     <meta name="description" content="Description of page" />
     <meta name="keywords" content="site, website, logo"/>
+    <script>
+        /* to prevent Firefox FOUC, this must be here */
+        let FF_FOUC_FIX;
+    </script>
     <link rel="stylesheet" href="<?= $assets ?>tailwind.css">
     <link rel="stylesheet" href="<?= $assets ?>style.css">
     <link rel="stylesheet" href="<?= $assets ?>icons.css">
     <script src="<?= $assets ?>bundle.js" type="module"></script>
+    <script>
+        // Helper function
+        let domReady = (cb) => {
+        	document.readyState === 'interactive' || document.readyState === 'complete' ? cb() : document.addEventListener('DOMContentLoaded', cb);
+        };
+
+        domReady(() => {
+	        // Display body when DOM is loaded
+	        document.body.style.visibility = 'visible';
+        });
+    </script>
 </head>
-<body class="overflow-x-hidden">
+<body class="group/body overflow-x-hidden" style="visibility: hidden">
+<script>0</script>
